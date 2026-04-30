d45058e01544b0a6f6849472963d7a54133dd48c
NicolasGomes99 <nicolasgabrielgomes9@gmail.com>
2026-04-30 13:40:32 -0300

    Configurando Rate Limiting

---
+++ src/main/java/br/edu/ufape/sgugatewayservice/config/RateLimiterConfig.java
@@ -0,0 +1,21 @@
+package br.edu.ufape.sgugatewayservice.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+@Configuration
+public class RateLimiterConfig {
+
+    @Bean
+    @Primary
+    public KeyResolver ipKeyResolver() {
+        return exchange -> Mono.just(
+                Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getAddress().getHostAddress()
+        );
+    }
+}
